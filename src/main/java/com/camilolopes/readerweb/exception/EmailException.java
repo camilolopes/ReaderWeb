1d168870997daf7fd4e3ce019cf37a4ffbeeae55
Camilo Lopes <camilo@camilolopes.com.br>
2013-06-04 20:37:53 -0300

    EmailException.java was missing

---
+++ src/main/java/com/camilolopes/readerweb/exception/EmailException.java
@@ -0,0 +1,16 @@
+package com.camilolopes.readerweb.exception;
+
+public class EmailException extends Exception {
+
+	private static final long serialVersionUID = -3550653196201940781L;
+	
+	
+	public EmailException() {
+	
+	}
+	public EmailException(String message) {
+		super(message);
+		
+	}
+	
+}
