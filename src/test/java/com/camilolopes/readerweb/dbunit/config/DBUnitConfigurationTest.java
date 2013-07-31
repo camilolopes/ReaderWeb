c3d2adb55502e6679db63ab84345e61179f52641
Camilo Lopes <camilo@camilolopes.com.br>
2013-07-31 18:28:33 -0300

    added unit test

--- src/test/java/com/camilolopes/readerweb/dbunit/config/DBUnitConfigurationTest.java
+++ src/test/java/com/camilolopes/readerweb/dbunit/config/DBUnitConfigurationTest.java
@@ -8,9 +8,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:**/OrderPersistenceTests-context.xml"})
-@TransactionConfiguration(defaultRollback=true,transactionManager="transactionManager")
-@Transactional
+//@ContextConfiguration(locations={"classpath:**/OrderPersistenceTests-context.xml"})
+//@TransactionConfiguration(defaultRollback=true,transactionManager="transactionManager")
+//@Transactional
 public class DBUnitConfigurationTest extends DBUnitConfiguration{
 	
 	@Before
